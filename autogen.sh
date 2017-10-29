bfef8408eb79dfcf56369821e4a22d02b15e0b72
Thomas Dreibholz <dreibh@simula.no>
2017-10-29 21:39:40 +0100

    Improved autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-./bootstrap && ./configure --prefix=/usr --enable-maintainer-mode && make
+./bootstrap
+./configure --prefix=/usr --enable-maintainer-mode
+
+# ------ Obtain number of cores ---------------------------------------------
+# Try Linux
+cores=`getconf _NPROCESSORS_ONLN 2>/dev/null`
+if [ "$cores" == "" ] ; then
+   # Try FreeBSD
+   cores=`sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2 | tr -d ' '`
+fi
+if [ "$cores" == "" ] ; then
+   cores="1"
+fi
+
+gmake -j$cores || make -j$cores
