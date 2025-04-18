968a8b6e83998051d2596fdfeeed804d051a17ea
Thomas Dreibholz <dreibh@simula.no>
2025-04-18 17:43:49 +0200

    Some clean-ups.

--- src/t1.cc
+++ src/t1.cc
@@ -1,3 +1,32 @@
+// ==========================================================================
+//                ____  _ _   _____   __  ______
+//                | __ )(_) |_|_   _|__\ \/ / ___|___  _ ____   __
+//                |  _ \| | '_ \| |/ _ \  / |   / _ \| '_ \ \ / /
+//                | |_) | | |_) | |  __//  \ |__| (_) | | | \ V /
+//                |____/|_|_.__/|_|\___/_/\_\____\___/|_| |_|\_/
+//
+//                          ---  BibTeX Converter  ---
+//                   https://www.nntb.no/~dreibh/bibtexconv/
+// ==========================================================================
+//
+// BibTeX Converter
+// Copyright (C) 2010-2025 by Thomas Dreibholz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: thomas.dreibholz@gmail.com
+
 #include <assert.h>
 #include <errno.h>
 #include <stdio.h>
@@ -23,14 +52,7 @@ class Mappings
                    const std::string& mappingFile,
                    const std::string& keyColumn,
                    const std::string& valueColumn);
-
-   inline const MappingEntry* findMapping(const std::string& mappingName) const {
-      std::map<const std::string, MappingEntry*>::const_iterator found = MappingSet.find(mappingName);
-      if(found != MappingSet.end()) {
-         return found->second;
-      }
-      return nullptr;
-   }
+   const MappingEntry* findMapping(const std::string& mappingName) const;
    const std::string& map(const MappingEntry* mappingEntry,
                           const std::string&  input) const;
 
@@ -223,6 +245,17 @@ bool Mappings::addMapping(const std::string& mappingName,
 
 
 // ###### Apply mapping #####################################################
+const MappingEntry* Mappings::findMapping(const std::string& mappingName) const
+{
+   std::map<const std::string, MappingEntry*>::const_iterator found = MappingSet.find(mappingName);
+   if(found != MappingSet.end()) {
+      return found->second;
+   }
+   return nullptr;
+}
+
+
+// ###### Apply mapping #####################################################
 const std::string& Mappings::map(const MappingEntry* mappingEntry,
                                  const std::string& input) const
 {
@@ -244,6 +277,7 @@ int main(int argc, char** argv)
 
    const MappingEntry* e = m.findMapping("author-urls");
    printf("=> %s\n", m.map(e, "Dreibholz, Thomas").c_str());
+   printf("=> %s\n", m.map(e, "Test, Thomas").c_str());
 
    return 0;
 }
