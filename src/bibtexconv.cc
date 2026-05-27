7bcc3688f067ba7120d1e95c740ec74057b53bcc
Thomas Dreibholz <dreibh@simula.no>
2026-05-27 23:55:06 +0200

    Options handling improvement.

--- src/bibtexconv.cc
+++ src/bibtexconv.cc
@@ -890,9 +890,17 @@ int main(int argc, char** argv)
             version();
           break;
          case 'h':
-            usage(argv[0], 0);
+         case '?':
+            // Exit with 0 on h/help, exit with 1 on '?' (unknown option):
+            usage(argv[0], (option == 'h') ? 0 : 1);
+          break;
+         case '-':
           break;
          default:
+            // This should not happen: wrong getopt parameters, or missing case?
+            fprintf(stderr, "INTERNAL ERROR: Unhandled option c=%c code=%x!\n",
+                    (isprint(option) ? (char)option : ' '), option);
+            return 1;
           break;
       }
    }
